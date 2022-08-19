dea44ad794148beef2f1382323ea0a813610bf13
JacksonSantin <jackdhanyelsn@gmail.com>
2022-08-19 10:49:10 -0300

    Projeto TODO

---
+++ src/components/TaskGrid.vue
@@ -0,0 +1,41 @@
+<template>
+  <div class="task-grid">
+    <template v-if="tasks.length">
+      <Task 
+        v-for="(task, i) in tasks" :key="task.name" 
+        @taskStateChanged="$emit('taskStateChanged', i)"
+        @taskDeleted="$emit('taskDeleted', i)"
+        :task="task"
+      ></Task>
+    </template>
+    <p v-else class="no-task">Sua vida está em dia :)</p>
+  </div>
+</template>
+
+<script>
+import Task from "./Task.vue";
+
+export default {
+  components: { Task },
+  props: {
+    tasks: { type: Array, required: true },
+  },
+};
+</script>
+
+<style>
+.task-grid {
+  display: flex;
+  justify-content: center;
+  flex-wrap: wrap;
+}
+
+.task-grid .task {
+  margin: 10px;
+}
+
+.no-task {
+  color: #aaa;
+  font-size: 1.7rem;
+}
+</style>
